a58722160f2522c79aa9780a5cd8184c1d9c7829
jldroid25 <37844922+jldroid25@users.noreply.github.com>
2020-12-09 00:27:46 -0500

    Update error_exceptions.py

--- 5-Scripting/error_exceptions.py
+++ 5-Scripting/error_exceptions.py
@@ -6,7 +6,7 @@ x = int(input('Enter a number: '))
 x += 20
 print(x)
 
-'''
+
 # This will throw a NameError
 #print(nonexistant_variable)
 
@@ -14,7 +14,50 @@ print(x)
 
 try:
     x = int(input('Enter a number: '))
+    
 except:
     print('That\'s not a valid number')
 
+   
 
+while True:
+    try:
+        x = int(input('Enter a number: '))
+        break
+    except:
+        print('That\'s not a valid number')
+
+    print('\nAttempted Input')
+
+
+# If you want code to keep running use a while-loop & 
+# break when correct input is entered 
+
+while True:
+    try:
+        x = int(input('Enter a number: '))
+        break
+    except:
+        print('That\'s not a valid number')
+
+    print('\nAttempted Input')
+
+'''
+
+# The "finally clause  is used to run a code we want to run before try-block
+# exit the code 
+
+while True:
+    try:
+        x = int(input('Enter a number: '))
+        break
+    # Throw exception  only when wrong input was entered
+    except ValueError:
+        print('That\'s not a valid number')
+    # ctrl + c to leave terminal
+    except KeyboardInterrupt:
+        print('\no input taken')
+        break
+
+    finally:
+        print('\nAttempted Input')
